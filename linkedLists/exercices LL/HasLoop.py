62b75f31ff66a3f9e773802b024d411738a0f95d
julio domanski <julio.domanski@hotmail.com>
2024-05-18 18:21:39 -0300

    terminado estudos LL, e 3 exercicios feitos LL

---
+++ linkedLists/exercices LL/HasLoop.py
@@ -0,0 +1,62 @@
+"""LL: Has Loop ( ** Interview Question)
+
+Write a method called has_loop that is part of the linked list class.
+
+The method should be able to detect if there is a cycle or loop present in the linked list.
+
+You are required to use Floyd's cycle-finding algorithm (also known as the "tortoise and the hare" algorithm) to detect the loop.
+
+This algorithm uses two pointers: a slow pointer and a fast pointer. The slow pointer moves one step at a time, while the fast pointer moves two steps at a time. If there is a loop in the linked list, the two pointers will eventually meet at some point. If there is no loop, the fast pointer will reach the end of the list.
+"""
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+
+    def has_loop(self):
+        slow_pointer = self.head
+        fast_pointer = self.head
+        
+        while slow_pointer != None and fast_pointer != None and fast_pointer.next!= None:
+            slow_pointer = slow_pointer.next
+            fast_pointer = fast_pointer.next.next
+            if slow_pointer == fast_pointer:
+                return True
+        return False
+    
+    
+    
+my_linked_list_1 = LinkedList(1)
+my_linked_list_1.append(2)
+my_linked_list_1.append(3)
+my_linked_list_1.append(4)
+my_linked_list_1.tail.next = my_linked_list_1.head
+print(my_linked_list_1.has_loop() ) # Returns True
+
+
+
+
+my_linked_list_2 = LinkedList(1)
+my_linked_list_2.append(2)
+my_linked_list_2.append(3)
+my_linked_list_2.append(4)
+print(my_linked_list_2.has_loop() ) # Returns False
